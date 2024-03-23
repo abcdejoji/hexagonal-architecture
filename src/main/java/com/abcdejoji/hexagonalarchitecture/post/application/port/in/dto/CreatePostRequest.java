745139f0a9a9def1ff047818728e818b532efb73
joji <abcdejoji@gmail.com>
2024-03-23 14:47:58 +0900

    [refactoring: CreatePostRequest] class -> record로 변경

--- src/main/java/com/abcdejoji/hexagonalarchitecture/post/application/port/in/dto/CreatePostRequest.java
+++ src/main/java/com/abcdejoji/hexagonalarchitecture/post/application/port/in/dto/CreatePostRequest.java
@@ -1,21 +1,15 @@
 package com.abcdejoji.hexagonalarchitecture.post.application.port.in.dto;
 
-import com.abcdejoji.hexagonalarchitecture.config.jacoco.*;
 import com.abcdejoji.hexagonalarchitecture.post.application.domain.*;
 import com.abcdejoji.hexagonalarchitecture.post.application.domain.vo.*;
 
-public class CreatePostRequest {
-
-    private final PostTitle postTitle;
-    private final PostContent postContent;
-
-    @ExcludeTestGenerated
-    public CreatePostRequest(PostTitle postTitle, PostContent postContent) {
-        this.postTitle = postTitle;
-        this.postContent = postContent;
-    }
+public record CreatePostRequest(
+        PostTitle postTitle,
+        PostContent postContent
+) {
 
     public Post toEntity() {
         return new Post(this.postTitle, this.postContent);
     }
+
 }
